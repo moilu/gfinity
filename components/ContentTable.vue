013cb2042464da0dcb19a9c3ec65b106551e08a1
moilu <moisesluna0390@gmail.com>
2023-08-22 18:11:49 -0700

    Update on player page

--- components/ContentTable.vue
+++ components/ContentTable.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="h-screen overflow-auto md:mx-auto pb-2 md:px-0 w-full md:w-[90%] 2xl:w-[55%]">
-  <table class="relative text-white w-full">
-    <thead class="bg-black sticky top-0">
+  <table class="relative w-full text-white">
+    <thead class="sticky top-0 bg-black">
         <tr>
           <template v-for="(header, idx) in table_headers">
             <th :key="idx" class="px-6 py-4 text-sm font-semibold text-center">
@@ -16,38 +16,38 @@
     <tbody>
         <template v-for="(card, idx) in fifaCards">
           <tr :key="idx" class="bg-[#101010] border-b border-black">
-            <NuxtLink to="/player">
-              <td class="whitespace-nowrap px-6 py-4 font-medium">{{ card.name }}</td>
+            <NuxtLink :to="{ path: '/player/' + card.slug.current, params: { id: card._id }}" >
+              <td class="px-6 py-4 font-medium whitespace-nowrap">{{ card.name }}</td>
             </NuxtLink>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="bg-white rounded p-1 text-black text-center">{{ card.rating }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center text-black bg-white rounded">{{ card.rating }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="bg-black rounded p-1 text-center">{{ card.position }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center bg-black rounded">{{ card.position }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="bg-black rounded p-1 text-center">{{ card.position }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center bg-black rounded">{{ card.position }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="border border-white rounded p-1 text-center">{{ card.statistics.pace.average }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center border border-white rounded">{{ card.statistics.pace.average }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="border border-white rounded p-1 text-center">{{ card.statistics.shooting.average }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center border border-white rounded">{{ card.statistics.shooting.average }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="border border-white rounded p-1 text-center">{{ card.statistics.passing.average }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center border border-white rounded">{{ card.statistics.passing.average }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="border border-white rounded p-1 text-center">{{ card.statistics.dribbling.average }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center border border-white rounded">{{ card.statistics.dribbling.average }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="border border-white rounded p-1 text-center">{{ card.statistics.defense.average }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center border border-white rounded">{{ card.statistics.defense.average }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="border border-white rounded p-1 text-center">{{ card.statistics.physical.average }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center border border-white rounded">{{ card.statistics.physical.average }}</div>
             </td>
-            <td class="whitespace-nowrap px-6 py-4">
-              <div class="bg-black rounded p-1 text-center">{{ `${card.workRatesAttacking[0]} / ${card.workRatesDefensive[0]}` }}</div>
+            <td class="px-6 py-4 whitespace-nowrap">
+              <div class="p-1 text-center bg-black rounded">{{ `${card.workRatesAttacking[0]} / ${card.workRatesDefensive[0]}` }}</div>
             </td>
           </tr>
         </template>
---
+++ pages/player/_slug.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    hi
+  </div>
+</template>
+<script>
+import { client } from '../../sanity.js'
+
+export default {
+  data() {
+    return {
+      fifaCards: []
+    }
+  },
+  methods: {
+    async getPosts() {
+      const fifaCards = await client.fetch('*[_slug == "aaron-ramsey-rangers-80-ovr"]');
+      return fifaCards
+    }
+  },
+  async created() {
+    this.fifaCards = await this.getPosts();
+  }
+}
+</script>
